d296ed979de2f88b236b030b32c4d1110e6bd2cf
skooze <kevinsmall93@gmail.com>
2026-01-23 15:57:37 -0700

    changing editor_State scoping

--- plugins/renderer/vulkan/src/vulkan_renderer.cpp
+++ plugins/renderer/vulkan/src/vulkan_renderer.cpp
@@ -1556,7 +1556,7 @@ bool vulkan_init(void* host) {
   hooks.physical_device = g_state.physical_device;
   hooks.device = g_state.device;
   hooks.queue = g_state.graphics_queue;
-  hooks.render_pass = g_state.dynamic_rendering_enabled ? nullptr : g_state.render_pass;
+  hooks.render_pass = g_state.render_pass;
   hooks.window = g_state.window;
   hooks.queue_family = g_state.graphics_queue_family;
   hooks.image_count = static_cast<uint32_t>(g_state.swapchain_images.size());
