18cdd6a6fc15cd05eaf8016d6b8084c22b0c4f35
skooze <kevinsmall93@gmail.com>
2026-02-05 11:34:22 -0700

    more changes to foot locking

--- core/src/locomotion.cpp
+++ core/src/locomotion.cpp
@@ -1235,6 +1235,38 @@ void update_procedural_gait(ecs::Registry& registry, ecs::Entity entity, float d
   gait->debug_target_lat_r = dot(sub(r_target_e, hips_e), side_e);
 
   if (gait->enable_ik) {
+    Vec3 lock_offset_e = v3();
+    int lock_count = 0;
+    const float max_reach = leg_len_e * 0.98f;
+    if (left_locked_now) {
+      const Vec3 lock_e = to_local_point(*transform, l_lock_w);
+      const Vec3 to_lock = sub(lock_e, hips_e);
+      const float dist = length(to_lock);
+      if (dist > max_reach && dist > kEps) {
+        lock_offset_e = add(lock_offset_e, mul(to_lock, (dist - max_reach) / dist));
+        ++lock_count;
+      }
+    }
+    if (right_locked_now) {
+      const Vec3 lock_e = to_local_point(*transform, r_lock_w);
+      const Vec3 to_lock = sub(lock_e, hips_e);
+      const float dist = length(to_lock);
+      if (dist > max_reach && dist > kEps) {
+        lock_offset_e = add(lock_offset_e, mul(to_lock, (dist - max_reach) / dist));
+        ++lock_count;
+      }
+    }
+    if (lock_count > 0) {
+      lock_offset_e = mul(lock_offset_e, 1.0f / static_cast<float>(lock_count));
+      const float max_offset = leg_len_e * 0.35f;
+      const float off_len = length(lock_offset_e);
+      if (off_len > max_offset && off_len > kEps) {
+        lock_offset_e = mul(lock_offset_e, max_offset / off_len);
+      }
+    } else {
+      lock_offset_e = v3();
+    }
+
     const float landing_window = 0.25f;
     float w_swing_l = 0.0f;
     float w_swing_r = 0.0f;
@@ -1255,8 +1287,11 @@ void update_procedural_gait(ecs::Registry& registry, ecs::Entity entity, float d
     const float desired_drop = std::min(0.0f, std::min(target_l.y - l_foot.y, target_r.y - r_foot.y));
     const float clamped_drop = clampf(desired_drop, -max_pelvis_drop, 0.0f);
     const float pelvis_alpha = 1.0f - std::exp(-dt * gait->ik_blend_speed);
+    gait->pelvis_offset[0] += (lock_offset_e.x - gait->pelvis_offset[0]) * pelvis_alpha;
     gait->pelvis_offset[1] += (clamped_drop - gait->pelvis_offset[1]) * pelvis_alpha;
-    add_pos(*skeleton, gait->bone_hips, 0.0f, gait->pelvis_offset[1], 0.0f);
+    gait->pelvis_offset[2] += (lock_offset_e.z - gait->pelvis_offset[2]) * pelvis_alpha;
+    add_pos(*skeleton, gait->bone_hips, gait->pelvis_offset[0],
+            gait->pelvis_offset[1], gait->pelvis_offset[2]);
 
     for (size_t i = 0; i < skeleton->bones.size(); ++i) {
       locals[i] = skeleton->bones[i].local_pose;
